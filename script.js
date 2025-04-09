ffb5d2f0cb51fe82888c724f3fed7f0b760beb26
Lochan-sharma232 <lochansharma537@gmail.com>
2025-04-09 17:46:32 +0530

    created a submission

--- script.js
+++
@@ -1,8 +0,0 @@
-// const form=document.querySelector("form-group");
-// const 
-// function adddData(e) {
-// 	const title=document.getElementById("title");
-// 	const author=document.getElementById("author");
-// 	const isbn=document.getElementById("isbn");
-// 	}
-// form.addEventListener("submit",adddData);
