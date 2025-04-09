8273cdcbc9f46d44ecd6807f079fa171cd222bd9
Lochan-sharma232 <lochansharma537@gmail.com>
2025-04-09 17:01:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+function addBook(){
+	const title=document.getElementById('title').value;
+	const author=document.getElementById('author').value;
+	const isbn=document.getElementById('isbn').value;
+}
+addBook.pushData()
