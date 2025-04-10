fe9a19214f771a67c3259f695dbbf5d4ca6488e0
Lochan-sharma232 <lochansharma537@gmail.com>
2025-04-10 13:20:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-document.getElementById("")
+document.getElementById("booklist").addEventListener("submit",function(event){
+event.preventDefault();
+
+	//get input values
+	const title=document.querySelector("#title").value;
+	const author=document.querySelector("#author").value;
+	const isbn=document.querySelector("#isbn").value;
+
+	//validation
+	if(!title || !author ||isbn){
+		alert("please fill in all fields");
+	    return;
+	}
+})
